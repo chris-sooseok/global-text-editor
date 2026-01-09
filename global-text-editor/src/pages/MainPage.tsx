043f1ac4a5cc735096b58f773948f6c561b0f71f
chris-sooseok <sooseokkim99@gmail.com>
2026-01-09 01:11:32 -0500

    switching to dev

--- global-text-editor/src/pages/MainPage.tsx
+++ global-text-editor/src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import SidebarRenderer from '../components/Sidebar/SidebarRenderer'
+import FileContent from '../components/FileContent/FileContent'
 
 function MainPage() {
     return (
         <>
             <SidebarRenderer />
+            <FileContent />
         </>
     )
 }
