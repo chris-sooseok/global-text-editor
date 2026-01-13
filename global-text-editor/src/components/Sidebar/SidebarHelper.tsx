8f19e47ead59833474ed5f9dec84713054ab6beb
chris-sooseok <sooseokkim99@gmail.com>
2026-01-12 23:56:05 -0500

    decent click behaviors of file/folder with new node creation

--- global-text-editor/src/components/Sidebar/SidebarHelper.tsx
+++ global-text-editor/src/components/Sidebar/SidebarHelper.tsx
@@ -23,8 +23,8 @@ export async function submitNewNodePromptHandler(
   newNodePromptInputRef: RefObject<HTMLInputElement | null>,
   newNodeType: 'folder' | 'file' | null,
   cancelNewNodePrompt: () => void,
-  selectedNode: SelectedNodeType,
-  selectNodeHandler: (node: SelectedNodeType) => void, // Dispath is a function that tkaes one argument and returns void
+  selectedFolder: SelectedNodeType,
+  selectFileHandler: (node: SelectedNodeType) => void, // Dispath is a function that tkaes one argument and returns void
   FsTree: {fsTree: FsTree},
   toggleFolderHandler: (nodeId: number) => void 
 ): Promise<void> {
@@ -44,8 +44,8 @@ export async function submitNewNodePromptHandler(
       return
     }
 
-    // check if new node has parent node, or is a root node
-    const parentId = selectedNode.id
+    // new node can only be created under selectedFolder or root
+    const parentId = selectedFolder.id
     const isRoot = parentId == null ? true : false
     const mimeType = computeMimeTypeFromName(trimmed)
     const res = await window.api.createFsNode(
@@ -62,7 +62,7 @@ export async function submitNewNodePromptHandler(
       const newFsNode: FsNode = FsTree.fsTree.insertNewNode(newNode)
       
       // highlight newly created node
-      selectNodeHandler(newFsNode)
+      selectFileHandler(newFsNode)
 
       // if newNode is a folder, toggle the folder
       if (newNode.type == 'folder') {
@@ -94,7 +94,7 @@ export function cancelNewNodePromptHandler(
 
 export function renderNewNodePromptHandler(
     newNodeType: 'folder' | 'file' | null,
-    selectedNode: SelectedNodeType,
+    selectedFolder: SelectedNodeType,
     depth: number,
     newNodePromptRef: RefObject<HTMLDivElement | null>,
     newNodePromptInputRef: RefObject<HTMLInputElement | null>,
@@ -104,7 +104,7 @@ export function renderNewNodePromptHandler(
   if (!newNodeType) return null
 
   return (
-    <li key={`__create_new_node_under__:${selectedNode.parentId ?? 'root'}:${newNodeType}`}>
+    <li key={`__create_new_node_under__:${selectedFolder.id ?? 'root'}:${newNodeType}`}>
       <div
         ref={newNodePromptRef}
         style={{
@@ -138,8 +138,9 @@ export function renderNewNodePromptHandler(
 export function renderNodeHandler(
   node: FsNode,
   depth: number,
-  selectedNode: SelectedNodeType,
-  selectNodeHandler: (node: SelectedNodeType) => void,
+  selectedFile: SelectedNodeType,
+  selectedFolder: SelectedNodeType,
+  selectNodeHandler:  (node: SelectedNodeType) => void,
   renderNode: (node: FsNode, depth?: number) => ReactNode,
   newNodeType: 'folder' | 'file' | null,
   toggledFolderIds: Set<number>,
@@ -149,7 +150,7 @@ export function renderNodeHandler(
   
   // File Node Row
   if (node.type == 'file') {
-    const isSelectedFile = selectedNode.id === node.id
+    const isSelectedFile = selectedFile.id === node.id
 
     return (
       <li key={node.id}>
@@ -161,21 +162,16 @@ export function renderNodeHandler(
             fontWeight: isSelectedFile ? 700 : 400,
             userSelect: 'none',
 
-            background: isSelectedFile ? 'rgba(59, 130, 246, 0.18)' : 'transparent',
+            background: isSelectedFile ? 'rgba(67, 102, 158, 0.18)' : 'transparent',
             borderRadius: 6,
             paddingTop: 2,
             paddingBottom: 2,  
         }}
-        onClick={() => {
-          // if file is already highlighted, return
-          if (isSelectedFile) return
-          
-          // if file is unhighlighted, highlight
-          if (!isSelectedFile){
-            selectNodeHandler(node)
-          }
-          
-        }}
+        onClick={() => clickFileHelper(
+          node, 
+          isSelectedFile,
+          selectNodeHandler
+        )}
         >
           <span style={{ display: 'inline-flex', alignItems: 'center', gap: 6 }}>
             <img
@@ -192,7 +188,7 @@ export function renderNodeHandler(
   }
 
   // Folder Node Row
-  const isSelectedFolder = selectedNode.id === node.id
+  const isSelectedFolder = selectedFolder.id === node.id
   const isExpanded = toggledFolderIds.has(node.id)
   const children = Array.isArray(node.children) ? node.children : []
 
@@ -205,32 +201,18 @@ export function renderNodeHandler(
           cursor: 'pointer',
           fontWeight: isSelectedFolder ? 700 : 400,
           userSelect: 'none',
-
-          background: isSelectedFolder ? 'rgba(59, 130, 246, 0.18)' : 'transparent', // light blue
+          background: isSelectedFolder ? 'rgba(138, 139, 141, 0.18)' : 'transparent', // light blue
           borderRadius: 6,
           paddingTop: 2,
           paddingBottom: 2,
         }}
-        onClick={() => {
-          // if folder is unfolded, but not highlighted, simply rehighlight it
-          if (!isSelectedFolder && isExpanded) {
-            selectNodeHandler(node)
-            return
-          }
-          
-          // if folder is unfolded, and highlighted, fold and unhighlight
-          if (isSelectedFolder && isExpanded) {
-            selectNodeHandler(EMPTY_SELECTED_NODE)
-            toggleFolderHandler(node.id)
-            return
-          }
-
-          // if folder is folded, and not highlighted, unfold and highlight
-          if (!isSelectedFolder && !isExpanded){
-            selectNodeHandler(node)
-            toggleFolderHandler(node.id)
-          }
-        }}
+        onClick={() => clickFolderHelper(
+          node,
+          isSelectedFolder,
+          selectNodeHandler,
+          isExpanded,
+          toggleFolderHandler
+        )}
       >
         <span style={{ display: 'inline-flex', alignItems: 'center', gap: 6 }}>
           <span aria-hidden="true" style={{ width: 12, display: 'inline-block' }}>
@@ -259,120 +241,160 @@ export function renderNodeHandler(
   )
 }
 
-export function renderNodeHandler2(
+function clickFileHelper(
   node: FsNode,
-  depth: number,
-  selectedNode: SelectedNodeType,
+  isSelectedFile: boolean,
   selectNodeHandler:  (node: SelectedNodeType) => void,
-  renderNode: (node: FsNode, depth?: number) => ReactNode,
-  newNodeType: 'folder' | 'file' | null,
-  toggledFolderIds: Set<number>,
-  toggleFolderHandler: (nodeId: number) => void,
-  renderNewNodePrompt: (depth: number) => ReactNode
-): ReactNode {
-
-  const isSelectedNode: boolean = selectedNode.id === node.id
-  const isSelectedFilesParent =
-  node.type === 'folder' &&
-  selectedNode.type === 'file' &&
-  selectedNode.parentId === node.id
-  let isExpanded: boolean = false
-  let children: FsNode[] = []
-
-
-  if (node.type == 'folder') {
-    isExpanded = toggledFolderIds.has(node.id)
-    children = node.children
+): void {
+  // if file is already highlighted, return
+  if (isSelectedFile) return
+  
+  // if file is unhighlighted, highlight
+  if (!isSelectedFile){
+    selectNodeHandler(node)
   }
-
-  return (
-    <li key={node.id}>
-        <div 
-          {...(node.type === 'folder' ? {'folder-node-row': 'true'} : {'file-node-row': 'true'})}
-          style={{ 
-            paddingLeft: (node.type === 'folder' ? depth * 5 : depth * 7),
-            cursor: 'pointer',
-            fontWeight: isSelectedNode ? 700 : 400,
-            userSelect: 'none',
-
-            background: isSelectedNode ? 'rgba(59, 130, 246, 0.18)' : 'transparent',
-            borderRadius: 6,
-            paddingTop: 2,
-            paddingBottom: 2,  
-        }}
-        onClick={() => clickOnNodeHelper(
-          node,
-          isSelectedNode,
-          selectNodeHandler,
-          isExpanded,
-          toggleFolderHandler
-        )}
-        >
-        <span style={{ display: 'inline-flex', alignItems: 'center', gap: 6 }}>
-            {node.type === 'folder' && (
-              <span aria-hidden="true" style={{ width: 12, display: 'inline-block' }}>
-                {isExpanded ? '▾' : '▸'}
-              </span>
-            )}
-            <img
-              src={(node.type === 'folder' ? folderIcon : fileIcon)}
-              alt=""
-              aria-hidden="true"
-              style={{ width: 18, height: 18, display: 'block' }}
-            />
-            <span>{node.name}</span>
-          </span>
-        </div>
-
-        {/* recursively render children node */}
-        {isExpanded && (
-          <ul style={{ margin: 0, paddingLeft: 8 }}>
-            {children.map((child) => renderNode(child, depth + 1))}
-
-            {/* folder prompt */}
-            {newNodeType && (isSelectedNode || isSelectedFilesParent)
-              ? renderNewNodePrompt(depth + 1)
-              : null}
-          </ul>
-        )}
-
-    </li> 
-  )
 }
 
-function clickOnNodeHelper(
-  node: FsNode, 
-  isSelectedNode: boolean,
+function clickFolderHelper(
+  node: FsNode,
+  isSelectedFolder: boolean,
   selectNodeHandler:  (node: SelectedNodeType) => void,
   isExpanded: boolean,
   toggleFolderHandler: (nodeId: number) => void
 ): void {
-  if (node.type === 'file') {
-      // if file is already highlighted, return
-      if (isSelectedNode) return
-      
-      // if file is unhighlighted, highlight
-      if (!isSelectedNode){
-        selectNodeHandler(node)
-      }
-  } else {
-    // if folder is unfolded, but not highlighted, simply rehighlight it
-    if (!isSelectedNode && isExpanded) {
+   if (!isSelectedFolder && isExpanded) {
       selectNodeHandler(node)
       return
     }
     
     // if folder is unfolded, and highlighted, fold and unhighlight
-    if (isSelectedNode && isExpanded) {
+    if (isSelectedFolder && isExpanded) {
       selectNodeHandler(EMPTY_SELECTED_NODE)
       toggleFolderHandler(node.id)
       return
     }
 
     // if folder is folded, and not highlighted, unfold and highlight
-    if (!isSelectedNode && !isExpanded){
+    if (!isSelectedFolder && !isExpanded){
       selectNodeHandler(node)
       toggleFolderHandler(node.id)
     }
-  }
 }
+
+
+// export function renderNodeHandler2(
+//   node: FsNode,
+//   depth: number,
+//   selectedFile: SelectedNodeType,
+//   selectedFolder: SelectedNodeType,
+//   selectNodeHandler:  (node: SelectedNodeType) => void,
+//   renderNode: (node: FsNode, depth?: number) => ReactNode,
+//   newNodeType: 'folder' | 'file' | null,
+//   toggledFolderIds: Set<number>,
+//   toggleFolderHandler: (nodeId: number) => void,
+//   renderNewNodePrompt: (depth: number) => ReactNode
+// ): ReactNode {
+
+//   const isSelectedFile: boolean = selectedFile.id === node.id
+//   const isSelectedFolder: boolean = selectedFolder.id === node.id
+//   let isExpanded: boolean = false
+//   let children: FsNode[] = []
+
+//   if (node.type == 'folder') {
+//     isExpanded = toggledFolderIds.has(node.id)
+//     children = node.children
+//   }
+
+//   return (
+//     <li key={node.id}>
+//         <div 
+//           {...(node.type === 'folder' ? {'folder-node-row': 'true'} : {'file-node-row': 'true'})}
+//           style={{ 
+//             paddingLeft: (node.type === 'folder' ? depth * 5 : depth * 7),
+//             cursor: 'pointer',
+//             fontWeight: (isSelectedNode) ? 700 : 400,
+//             userSelect: 'none',
+
+//             background: isSelectedNode ? 'rgba(59, 130, 246, 0.18)' : 'transparent',
+//             borderRadius: 6,
+//             paddingTop: 2,
+//             paddingBottom: 2,  
+//         }}
+//         onClick={() => clickOnNodeHelper(
+//           node,
+//           isSelectedFile,
+//           isSelectedFolder,
+//           selectNodeHandler,
+//           isExpanded,
+//           toggleFolderHandler
+//         )}
+//         >
+//         <span style={{ display: 'inline-flex', alignItems: 'center', gap: 6 }}>
+//             {node.type === 'folder' && (
+//               <span aria-hidden="true" style={{ width: 12, display: 'inline-block' }}>
+//                 {isExpanded ? '▾' : '▸'}
+//               </span>
+//             )}
+//             <img
+//               src={(node.type === 'folder' ? folderIcon : fileIcon)}
+//               alt=""
+//               aria-hidden="true"
+//               style={{ width: 18, height: 18, display: 'block' }}
+//             />
+//             <span>{node.name}</span>
+//           </span>
+//         </div>
+
+//         {/* recursively render children node */}
+//         {isExpanded && (
+//           <ul style={{ margin: 0, paddingLeft: 8 }}>
+//             {children.map((child) => renderNode(child, depth + 1))}
+
+//             {/* folder prompt */}
+//             {newNodeType && isSelectedNode
+//               ? renderNewNodePrompt(depth + 1)
+//               : null}
+//           </ul>
+//         )}
+
+//     </li> 
+//   )
+// }
+
+// function clickOnNodeHelper(
+//   node: FsNode, 
+//   isSelectedFile: boolean,
+//   isSelectedFolder: boolean,
+//   selectNodeHandler:  (node: SelectedNodeType) => void,
+//   isExpanded: boolean,
+//   toggleFolderHandler: (nodeId: number) => void
+// ): void {
+//   if (node.type === 'file') {
+//       // if file is already highlighted, return
+//       if (isSelectedFile) return
+      
+//       // if file is unhighlighted, highlight
+//       if (!isSelectedFile){
+//         selectNodeHandler(node)
+//       }
+//   } else {
+//     // if folder is unfolded, but not highlighted, simply rehighlight it
+//     if (!isSelectedFolder && isExpanded) {
+//       selectNodeHandler(node)
+//       return
+//     }
+    
+//     // if folder is unfolded, and highlighted, fold and unhighlight
+//     if (isSelectedFolder && isExpanded) {
+//       selectNodeHandler(EMPTY_SELECTED_NODE)
+//       toggleFolderHandler(node.id)
+//       return
+//     }
+
+//     // if folder is folded, and not highlighted, unfold and highlight
+//     if (!isSelectedFolder && !isExpanded){
+//       selectNodeHandler(node)
+//       toggleFolderHandler(node.id)
+//     }
+//   }
+// }
