30966b267327a63cb0eaaeb4d0f4f2c41dd801df
chris-sooseok <sooseokkim99@gmail.com>
2026-01-27 03:08:48 -0500

    implmented images

--- global-text-editor/electron/main.cjs
+++ global-text-editor/electron/main.cjs
@@ -1,6 +1,15 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, protocol} = require('electron')
 const path = require('node:path')
 const { migrate, close_db } = require("./db/index.cjs");
+
+
+// images
+protocol.registerSchemesAsPrivileged([
+  {
+    scheme: "gtext",
+    privileges: { standard: true, secure: true, supportFetchAPI: true },
+  },
+])
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -27,6 +36,21 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
+    // ✅ 2) register protocol handler
+  protocol.registerFileProtocol("gtext", (request, callback) => {
+    try {
+      const u = new URL(request.url)
+      // gtext://nodes/<uuid>/assets/<file>
+      const rel = path.posix.join(u.host, u.pathname)
+      const relPath = decodeURIComponent(rel)
+      const absPath = path.join(app.getPath("userData"), relPath)
+      callback({ path: absPath })
+    } catch (err) {
+      console.error("[gtext protocol] failed:", err)
+      callback({ error: -2 })
+    }
+  })
+
   migrate() // ensure migrating all sqls
   require("./ipc/fsNodeIpc.cjs")
   require("./ipc/editorIpc.cjs")
